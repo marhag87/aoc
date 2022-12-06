e80a4c31bcf4afee3b240b637fe7f8a0193f055c
Martin Hagström <marhag87@gmail.com>
2022-12-06 12:31:19 +0100

    Fix warnings

--- 2015/src/day_5.rs
+++ 2015/src/day_5.rs
@@ -6,10 +6,7 @@ pub(crate) fn day_5_1(input: &str) -> usize {
         .filter(|line| {
             let at_least_three_vowels = line
                 .chars()
-                .filter(|char| match char {
-                    'a' | 'e' | 'i' | 'o' | 'u' => true,
-                    _ => false,
-                })
+                .filter(|char| matches!(char, 'a' | 'e' | 'i' | 'o' | 'u'))
                 .count()
                 >= 3;
             let double_char = line.chars().tuple_windows().any(|(a, b)| a == b);
@@ -29,7 +26,7 @@ pub(crate) fn day_5_2(input: &str) -> usize {
             let appears_twice = line
                 .chars()
                 .tuple_windows()
-                .any(|(a, b, c)| line.matches(&format!("{}{}", a, b)).count() >= 2);
+                .any(|(a, b)| line.matches(&format!("{}{}", a, b)).count() >= 2);
             let repeats_with_other_between = line
                 .chars()
                 .tuple_windows()
