c13a9c253ad1ef8223cf0a4f07568ecc656636b5
Martin Hagström <marhag87@gmail.com>
2022-12-11 21:11:58 +0100

    Clean up

--- 2015/src/day_7.rs
+++ 2015/src/day_7.rs
@@ -5,14 +5,14 @@ use itertools::Itertools;
 #[derive(Debug)]
 enum Target {
     Register(String),
-    Direct(usize),
+    Number(usize),
 }
 
 impl Target {
     fn new(s: &str, n: usize) -> Self {
         match s.split(' ').nth(n).unwrap().parse::<usize>() {
-            Ok(num) => Target::Direct(num),
-            Err(_) => Target::Register(s.split(' ').nth(n).unwrap().to_string()),
+            Ok(num) => Self::Number(num),
+            Err(_) => Self::Register(s.split(' ').nth(n).unwrap().to_string()),
         }
     }
 }
@@ -29,22 +29,19 @@ enum Command {
 
 impl From<&str> for Command {
     fn from(s: &str) -> Self {
+        use Command::*;
         match s
             .split(' ')
             .take(2)
             .collect_tuple()
             .expect("should have tuples")
         {
-            (_, "RSHIFT") => {
-                Command::Rshift(Target::new(s, 0), Target::new(s, 2), Target::new(s, 4))
-            }
-            (_, "LSHIFT") => {
-                Command::Lshift(Target::new(s, 0), Target::new(s, 2), Target::new(s, 4))
-            }
-            (_, "OR") => Command::Or(Target::new(s, 0), Target::new(s, 2), Target::new(s, 4)),
-            (_, "->") => Command::Direct(Target::new(s, 0), Target::new(s, 2)),
-            ("NOT", _) => Command::Not(Target::new(s, 1), Target::new(s, 3)),
-            (_, "AND") => Command::And(Target::new(s, 0), Target::new(s, 2), Target::new(s, 4)),
+            (_, "RSHIFT") => Rshift(Target::new(s, 0), Target::new(s, 2), Target::new(s, 4)),
+            (_, "LSHIFT") => Lshift(Target::new(s, 0), Target::new(s, 2), Target::new(s, 4)),
+            (_, "OR") => Or(Target::new(s, 0), Target::new(s, 2), Target::new(s, 4)),
+            (_, "->") => Direct(Target::new(s, 0), Target::new(s, 2)),
+            ("NOT", _) => Not(Target::new(s, 1), Target::new(s, 3)),
+            (_, "AND") => And(Target::new(s, 0), Target::new(s, 2), Target::new(s, 4)),
             _ => {
                 panic!("invalid command")
             }
@@ -53,44 +50,29 @@ impl From<&str> for Command {
 }
 
 pub(crate) fn day_7_1(input: &str) -> usize {
+    use Command::*;
+    use Target::*;
     let commands = input
         .lines()
         .map(|line| line.into())
         .collect::<Vec<Command>>();
     let mut registers: HashMap<&String, Option<usize>> = HashMap::new();
     commands.iter().for_each(|command| match command {
-        Command::Rshift(Target::Register(a), _, Target::Register(b)) => {
+        Command::Direct(Number(_), Register(a)) => {
+            registers.insert(a, None);
+        }
+        Rshift(Register(a), _, Register(b))
+        | Lshift(Register(a), _, Register(b))
+        | Direct(Register(a), Register(b))
+        | Not(Register(a), Register(b))
+        | And(Number(_), Register(a), Register(b)) => {
             registers.insert(a, None);
             registers.insert(b, None);
         }
-        Command::Lshift(Target::Register(a), _, Target::Register(b)) => {
-            registers.insert(a, None);
-            registers.insert(b, None);
-        }
-        Command::Or(Target::Register(a), Target::Register(b), Target::Register(c)) => {
-            registers.insert(a, None);
-            registers.insert(b, None);
-            registers.insert(c, None);
-        }
-        Command::Direct(Target::Register(a), Target::Register(b)) => {
-            registers.insert(a, None);
-            registers.insert(b, None);
-        }
-        Command::Direct(Target::Direct(_), Target::Register(a)) => {
-            registers.insert(a, None);
-        }
-        Command::Not(Target::Register(a), Target::Register(b)) => {
-            registers.insert(a, None);
-            registers.insert(b, None);
-        }
-        Command::And(Target::Register(a), Target::Register(b), Target::Register(c)) => {
+        Or(Register(a), Register(b), Register(c)) | And(Register(a), Register(b), Register(c)) => {
             registers.insert(a, None);
             registers.insert(b, None);
             registers.insert(c, None);
-        }
-        Command::And(Target::Direct(_), Target::Register(a), Target::Register(b)) => {
-            registers.insert(a, None);
-            registers.insert(b, None);
         }
         _ => {
             panic!("invalid command");
@@ -98,51 +80,50 @@ pub(crate) fn day_7_1(input: &str) -> usize {
     });
     while registers.iter().any(|(_, v)| v.is_none()) {
         commands.iter().for_each(|command| match command {
-            Command::Rshift(Target::Register(a), Target::Direct(b), Target::Register(c)) => {
+            Rshift(Register(a), Number(b), Register(c)) => {
                 if let Some(Some(a)) = registers.get(a) {
                     registers.insert(c, Some(a >> b));
                 };
             }
-            Command::Lshift(Target::Register(a), Target::Direct(b), Target::Register(c)) => {
+            Lshift(Register(a), Number(b), Register(c)) => {
                 if let Some(Some(a)) = registers.get(a) {
                     registers.insert(c, Some(a << b));
                 };
             }
-            Command::Or(Target::Register(a), Target::Register(b), Target::Register(c)) => {
+            Or(Register(a), Register(b), Register(c)) => {
                 if let Some(Some(a)) = registers.get(a) {
                     if let Some(Some(b)) = registers.get(b) {
                         registers.insert(c, Some(a | b));
                     }
                 };
             }
-            Command::Direct(Target::Register(a), Target::Register(b)) => {
+            Direct(Register(a), Register(b)) => {
                 if let Some(a) = registers.get(a) {
-                    registers.insert(b, a.clone());
+                    registers.insert(b, *a);
                 };
             }
-            Command::Direct(Target::Direct(a), Target::Register(b)) => {
-                registers.insert(b, Some(a.clone()));
+            Direct(Number(a), Register(b)) => {
+                registers.insert(b, Some(*a));
             }
-            Command::Not(Target::Register(a), Target::Register(b)) => {
+            Not(Register(a), Register(b)) => {
                 if let Some(Some(a)) = registers.get(a) {
-                    registers.insert(b, Some(!a.clone()));
+                    registers.insert(b, Some(!*a));
                 };
             }
-            Command::And(Target::Register(a), Target::Register(b), Target::Register(c)) => {
+            And(Register(a), Register(b), Register(c)) => {
                 if let Some(Some(a)) = registers.get(a) {
                     if let Some(Some(b)) = registers.get(b) {
                         registers.insert(c, Some(a & b));
                     }
                 };
             }
-            Command::And(Target::Direct(a), Target::Register(b), Target::Register(c)) => {
+            And(Number(a), Register(b), Register(c)) => {
                 if let Some(Some(b)) = registers.get(b) {
                     registers.insert(c, Some(a & b));
                 }
             }
             _ => {
-                eprintln!("command = {:?}", command);
-                panic!("help")
+                panic!("invalid command")
             }
         });
     }
